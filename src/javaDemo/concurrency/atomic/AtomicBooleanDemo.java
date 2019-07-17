7ae880845e76acd8841839cf61a6ae5a2fe38f8f
fhp45 <948592025@qq.com>
2019-07-17 19:17:22 +0800

    AtomicBoolean可以避免boolean的比较和赋值的非原子性导致的错误

--- src/javaDemo/concurrency/atomic/AtomicBooleanDemo.java
+++ src/javaDemo/concurrency/atomic/AtomicBooleanDemo.java
@@ -1,6 +1,7 @@
 package javaDemo.concurrency.atomic;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * AtomicBoolean中最经常使用的一个方法是compareAndSet，通过这个原子操作可以避免boolean常规使用中先判断后赋值的非原子性的错误。
@@ -8,36 +9,36 @@ import java.util.concurrent.TimeUnit;
  * 这个例子还可以用来让两个线程循环输出0/1
  */
 public class AtomicBooleanDemo {
-    public boolean isTrue = true;
+    private boolean isTrue = true;
+
+    private AtomicBoolean atomicTrue = new AtomicBoolean(true);
 
     //使用非原子boolean时，会由于判断和赋值的飞原子性，出现并发错误
     public void alwaysTrue() {
         //下面的代码看起来应该是永远输出true的，运行一下你就知道
         if (isTrue) {
-            try {
-                //让线程暂停20ms
-                TimeUnit.MILLISECONDS.sleep(20);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             System.out.println(isTrue == true);
             isTrue = false;
         } else {
-            try {
-                //让线程暂停20ms
-                TimeUnit.MILLISECONDS.sleep(20);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             System.out.println(isTrue == false);
             isTrue = true;
         }
     }
 
+    //使用AtomicBoolean时，避免了在判断和修改之间时发生并发错误
+    public void alwaysTrueAtomic(){
+        if(atomicTrue.compareAndSet(true,false)){
+            System.out.println(atomicTrue.get() == false);
+        }else{
+            atomicTrue.compareAndSet(false,true);
+            System.out.println(atomicTrue.get() == true);
+        }
+    }
+
     public static void main(String[] args) {
         AtomicBooleanDemo demo = new AtomicBooleanDemo();
-
-        for (int i = 0; i < 1000; i++) {
+        //启动一百个线程，我们会发现输出并不全是true
+        for (int i = 0; i < 100; i++) {
                 new Thread(demo::alwaysTrue).start();
         }
     }
